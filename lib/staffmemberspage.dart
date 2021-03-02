c1fa8d16dc6dee1c37b1df009e7055a77144b11d
Claire Casey <clcasey805@gmail.com>
2021-03-02 11:29:05 -0800

    update nav bar for staff member page

--- lib/staffmemberspage.dart
+++ lib/staffmemberspage.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:rithm_sis/homepage.dart';
 import './staffdata.dart';
+import './homepage.dart';
 
 class StaffMembersPage extends StatefulWidget {
   static const String routeName = "/staff";
-  
+
   @override
   _StaffDataState createState() => _StaffDataState();
 }
@@ -13,8 +15,10 @@ class _StaffDataState extends State<StaffMembersPage> {
 
   Widget _buildStaffMembersPage() {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
+      // crossAxisAlignment: CrossAxisAlignment.center,
+      // mainAxisAlignment: MainAxisAlignment.center,
       // mainAxisSize: MainAxisSize.min,
+      
       children: <Widget>[
         // Text('Upcoming Lectures/ Exercises:'),
         Expanded(
@@ -26,6 +30,7 @@ class _StaffDataState extends State<StaffMembersPage> {
 
   Widget _buildStaffList() {
     return ListView.separated(
+        padding: EdgeInsets.only(top: 20),
         itemCount: _staffData.length,
         separatorBuilder: (BuildContext context, int index) => Divider(),
         itemBuilder: (context, item) {
@@ -34,9 +39,19 @@ class _StaffDataState extends State<StaffMembersPage> {
   }
 
   Widget _buildStaffDetail(Map staffMember) {
+    final String full_name = staffMember['first_name'] + " " + staffMember['last_name'];
     return ListTile(
-      trailing: Image.network(staffMember['photo_small_url']),
-      title: Text(staffMember['title']),
+      leading: 
+          Container(
+            width: 50,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: NetworkImage(staffMember['photo_small_url']),
+              ),
+            ),
+          ),
+      title: Text(full_name),
       subtitle: Text(staffMember['description']),
       isThreeLine: true,
     );
@@ -48,16 +63,21 @@ class _StaffDataState extends State<StaffMembersPage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        leading: Image.asset(
-          'assets/rithm.png',
-        ),
+        // leading: Image.asset(
+        //   'assets/rithm.png',
+        // ),
         titleSpacing: 30.0,
         centerTitle: true,
         leadingWidth: 100.0,
         title: Text('{R} Cohort 19'),
-        // actions: <Widget>[
-        //   IconButton(icon: Icon(Icons.list), onPressed: () => {})
-        // ]
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.home), onPressed: () => {
+            Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Homepage()),
+                )
+          })
+        ]
       ),
       body: _buildStaffMembersPage(),
     );
